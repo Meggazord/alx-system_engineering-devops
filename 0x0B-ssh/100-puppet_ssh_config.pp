4285ad16f9772bc3b450e3fbfa4362f33782a440
Meggazord <k_megahed@hotmail.com>
2024-03-27 20:46:28 +0200

    foxdeD

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Do the ssh configuration file
+include stdlib
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/sshd_config',
+  line    => 'HostKey ~/.ssh/school',
+  match   => '^#?HostKey \/etc\/ssh\/ssh_host_rsa_key$',
+  replace => true,
+}
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/sshd_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication.+$',
+  replace => true,
+}

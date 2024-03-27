2a5d15d0548f9050d3638e9024a399f12a8db5ea
Meggazord <k_megahed@hotmail.com>
2024-03-27 19:23:40 +0200

    xifef

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Puppet manifest to install Flask package
-
 package { 'flask':
   ensure   => '2.1.0',
+  name     => 'flask',
   provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
 # Puppet manifest to kill a process named "killmenow"
-
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/usr/bin', '/bin'],
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
+exec { 'terminate process':
+  command  => 'pkill killmenow',
+  provider => 'shell'
 }

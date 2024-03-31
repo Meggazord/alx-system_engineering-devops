7fffbb6681194485657cd4e8fb8ba7448245a5ff
Meggazord <k_megahed@hotmail.com>
2024-03-31 22:53:20 +0200

    fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,47 @@
-# 2-puppet_custom_http_response_header.pp
+# puppet file to install custom http_response_header
+
+exec { 'apt_update':
+  command => '/usr/bin/apt update',
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+$nginx_config_content = @(EOF)
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+        add_header X-Served-By $hostname;
+    }
+}
+EOF
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => $nginx_config_content,
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+
+exec { 'apt_update':
+  command => '/usr/bin/apt update',
+}
 
 package { 'nginx':
   ensure => installed,

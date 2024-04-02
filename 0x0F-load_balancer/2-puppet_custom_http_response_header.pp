d12b72d600d2234df250a19545266caa5d83e1d2
Meggazord <k_megahed@hotmail.com>
2024-04-02 22:31:49 +0200

    fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,20 +8,21 @@ package { 'nginx':
   ensure => installed,
 }
 
+$hostname = $facts['hostname']
+
 $nginx_config_content = @(EOF)
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
 
     root /var/www/html;
-
     index index.html index.htm index.nginx-debian.html;
 
     server_name _;
 
     location / {
         try_files $uri $uri/ =404;
-        add_header X-Served-By $hostname;
+        add_header X-Served-By ${hostname};
     }
 }
 EOF
@@ -36,39 +37,4 @@ service { 'nginx':
   ensure  => running,
   enable  => true,
   require => Package['nginx'],
-}
-
-
-exec { 'apt_update':
-  command => '/usr/bin/apt update',
-}
-
-package { 'nginx':
-  ensure => installed,
-}
-
-$hostname = $facts['hostname']
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        try_files $uri $uri/ =404;
-        add_header X-Served-By ${hostname};
-    }
-}",
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
 }

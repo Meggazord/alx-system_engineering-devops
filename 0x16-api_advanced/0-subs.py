c79981e2388cf873c2b085a947c955b7e1190d2a
Meggazord <k_megahed@hotmail.com>
2024-05-28 14:27:22 +0300

    xedi

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Return the number of subscribers to a given subreddit """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code !== 200:
+        return 0
+    else:
         data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers'], "OK" 
-    return 0
+        if 'data' not in data:
+            return 0
+        if 'subscribers' not in data.get('data'):
+            return 0
+        return data.json()['data']['subscribers']
+
  

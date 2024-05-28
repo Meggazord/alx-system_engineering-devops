f13e42a6c6827d9c00d9d415a7c35e92cbd10fd8
Meggazord <k_megahed@hotmail.com>
2024-05-28 14:34:23 +0300

    gixedf

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Get number of top 10 posts from Reddit
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json()
+    posts = data.get('data', {}).get('children', [])
+    if not posts:
+        print(None)
+        return
+    for post in posts:
+        print(post['data']['title'])

1da01dcbcbd18ccacfcffba950e659fe6254d146
Meggazord <k_megahed@hotmail.com>
2024-05-28 14:30:29 +0300

    fxied

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """
-Get number of users from Reddit 
+Get number of users from Reddit
 """
 
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
-    """ Return the number of subscribers to a given subreddit """
+    """Returns the number of subscribers to a given subreddit.
 
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers if the subreddit is valid, otherwise 0.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code !== 200:
+    if response.status_code != 200:
         return 0
-    else:
-        data = response.json()
-        if 'data' not in data:
-            return 0
-        if 'subscribers' not in data.get('data'):
-            return 0
-        return data.json()['data']['subscribers']
-
- 
+    data = response.json()
+    if 'data' not in data or 'subscribers' not in data['data']:
+        return 0
+    return data['data']['subscribers']

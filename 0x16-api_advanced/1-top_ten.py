aadc7e8acb93e6f4fd34d9a3c7f0b7dbfb4de684
Meggazord <k_megahed@hotmail.com>
2024-05-28 14:35:45 +0300

    fxied

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,6 @@
 """
 Get number of top 10 posts from Reddit
 """
-
 import requests
 
 def top_ten(subreddit):
@@ -11,13 +10,20 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    try:
+        data = response.json()
+    except ValueError:
         print(None)
         return
-    data = response.json()
-    posts = data.get('data', {}).get('children', [])
+
+    if response.status_code != 200 or 'data' not in data or 'children' not in data['data']:
+        print(None)
+        return
+
+    posts = data['data']['children']
     if not posts:
         print(None)
         return
+
     for post in posts:
         print(post['data']['title'])
